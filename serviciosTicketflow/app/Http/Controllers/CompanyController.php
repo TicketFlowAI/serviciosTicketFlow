0a276848ab0149fed0549509889a18ae6e2f158b
Dev Server <dennisocana@gmail.com>
2024-10-06 14:25:03 -0400

    Added the routes for companies to the api routes file and finished the controller for companies adding basic crud functions

--- serviciosTicketflow/app/Http/Controllers/CompanyController.php
+++ serviciosTicketflow/app/Http/Controllers/CompanyController.php
@@ -4,16 +4,28 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreCompanyRequest;
 use App\Http\Requests\UpdateCompanyRequest;
+use App\Http\Resources\CompanyResource;
+use App\Interfaces\CompanyRepositoryInterface;
+use App\Classes\ApiResponseClass;
+use Illuminate\Support\Facades\DB;
 use App\Models\Company;
 
 class CompanyController extends Controller
 {
+    private CompanyRepositoryInterface $companyRepositoryInterface;
+    
+    public function __construct(CompanyRepositoryInterface $companyRepositoryInterface)
+    {
+        $this->companyRepositoryInterface = $companyRepositoryInterface;
+    }
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        //
+        $data = $this->companyRepositoryInterface->index();
+
+        return ApiResponseClass::sendResponse(CompanyResource::collection($data),'',200);
     }
 
     /**
@@ -29,15 +41,30 @@ class CompanyController extends Controller
      */
     public function store(StoreCompanyRequest $request)
     {
-        //
+        $details =[
+            'name' => $request->name,
+            'details' => $request->details
+        ];
+        DB::beginTransaction();
+        try{
+             $company = $this->companyRepositoryInterface->store($details);
+
+             DB::commit();
+             return ApiResponseClass::sendResponse(new CompanyResource($company),'Company Create Successful',201);
+
+        }catch(\Exception $ex){
+            return ApiResponseClass::rollback($ex);
+        }
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Company $company)
+    public function show($id)
     {
-        //
+        $company = $this->companyRepositoryInterface->getById($id);
+
+        return ApiResponseClass::sendResponse(new CompanyResource($company),'',200);
     }
 
     /**
@@ -51,16 +78,31 @@ class CompanyController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdateCompanyRequest $request, Company $company)
+    public function update(UpdateCompanyRequest $request, $id)
     {
-        //
+        $updateDetails =[
+            'name' => $request->name,
+            'details' => $request->details
+        ];
+        DB::beginTransaction();
+        try{
+             $company = $this->companyRepositoryInterface->update($updateDetails,$id);
+
+             DB::commit();
+             return ApiResponseClass::sendResponse('Company Update Successful','',201);
+
+        }catch(\Exception $ex){
+            return ApiResponseClass::rollback($ex);
+        }
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Company $company)
+    public function destroy($id)
     {
-        //
+         $this->companyRepositoryInterface->delete($id);
+
+        return ApiResponseClass::sendResponse('Company Delete Successful','',204);
     }
 }
--- serviciosTicketflow/routes/api.php
+++ serviciosTicketflow/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CompanyController;
 use App\Http\Resources\UserResource;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -8,3 +9,9 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 //Route::group(['middleware' => ['role:manager']], function () { ... });
+
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    // Protected Company API routes
+    Route::apiResource('/companies',CompanyController::class)->middleware('auth:sanctum');
+});
