f61a4db2f97484dee121498bdc02fba7a42a2ef3
Dev Server <dennisocana@gmail.com>
2024-10-06 18:37:17 -0400

    modified the database strcuture to add a length and unique atribute

--- serviciosTicketflow/database/migrations/2024_10_06_205143_create_categories_table.php
+++ serviciosTicketflow/database/migrations/2024_10_06_205143_create_categories_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->string('category')->unique();
+            $table->string('category',length:50)->unique();
             $table->timestamps();
         });
     }
