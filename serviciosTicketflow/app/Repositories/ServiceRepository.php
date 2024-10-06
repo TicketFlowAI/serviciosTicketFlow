eaa1ca274cb5cdf4f6b9beeca60213d5d5d453af
Dev Server <dennisocana@gmail.com>
2024-10-06 18:00:13 -0400

    Added comments for functions

--- serviciosTicketflow/app/Repositories/ServiceRepository.php
+++ serviciosTicketflow/app/Repositories/ServiceRepository.php
@@ -6,26 +6,51 @@ use App\Models\Service;
 
 class ServiceRepository implements ServiceRepositoryInterface
 {
-    /**
-     * Create a new class instance.
-     */
-    public function index(){
-        return Service::all();
-    }
+   /**
+    * Create a new class instance.
+    */
 
-    public function getById($id){
-       return Service::findOrFail($id);
-    }
+   public function __construct()
+   {
+      //
+   }
+   /**
+    * Retrieves all items.
+    */
+   public function index()
+   {
+      return Service::all();
+   }
 
-    public function store(array $data){
-       return Service::create($data);
-    }
+   /**
+    * Retrieves an item by ID.
+    */
+   public function getById($id)
+   {
+      return Service::findOrFail($id);
+   }
 
-    public function update(array $data,$id){
-       return Service::whereId($id)->update($data);
-    }
-    
-    public function delete($id){
-       Service::destroy($id);
-    }
+   /**
+    * Creates a new item in the repository.
+    */
+   public function store(array $data)
+   {
+      return Service::create($data);
+   }
+
+   /**
+    * Updates an Item by ID.
+    */
+   public function update(array $data, $id)
+   {
+      return Service::whereId($id)->update($data);
+   }
+
+   /**
+    * Deletes an Item by ID.
+    */
+   public function delete($id)
+   {
+      Service::destroy($id);
+   }
 }
