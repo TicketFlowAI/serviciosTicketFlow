5ac580ce7e63404b694ab8f16c57b370cbc8f68b
Dev Server <dennisocana@gmail.com>
2024-10-06 17:06:14 -0400

    Service repository and interface implemented

---
+++ serviciosTicketflow/app/Repositories/ServiceRepository.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Repositories;
+use App\Interfaces\Interfaces\ServiceRepositoryInterface;
+use App\Models\Service;
+
+class ServiceRepository implements ServiceRepositoryInterface
+{
+    /**
+     * Create a new class instance.
+     */
+    public function index(){
+        return Service::all();
+    }
+
+    public function getById($id){
+       return Service::findOrFail($id);
+    }
+
+    public function store(array $data){
+       return Service::create($data);
+    }
+
+    public function update(array $data,$id){
+       return Service::whereId($id)->update($data);
+    }
+    
+    public function delete($id){
+       Service::destroy($id);
+    }
+}
