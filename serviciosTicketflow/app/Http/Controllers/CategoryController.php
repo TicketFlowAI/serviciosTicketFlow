ba2ab161982c12837c013ffc986219d24acd3613
Dev Server <dennisocana@gmail.com>
2024-10-06 18:31:31 -0400

    Added a scaffold category controller and added the raw data.

--- serviciosTicketflow/app/Http/Controllers/CategoryController.php
+++ serviciosTicketflow/app/Http/Controllers/CategoryController.php
@@ -2,18 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use App\Http\Requests\StoreCategoryRequest;
 use App\Http\Requests\UpdateCategoryRequest;
-use App\Models\Category;
-
+use App\Interfaces\CategoryRepositoryInterface;
+use App\Classes\ApiResponseClass;
+use App\Http\Resources\CategoryResource;
+use Illuminate\Support\Facades\DB;
 class CategoryController extends Controller
 {
+    
+    private CategoryRepositoryInterface $categoryRepositoryInterface;
+    
+    public function __construct(CategoryRepositoryInterface $categoryRepositoryInterface)
+    {
+        $this->categoryRepositoryInterface = $categoryRepositoryInterface;
+    }
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        //
+        $data = $this->categoryRepositoryInterface->index();
+
+        return ApiResponseClass::sendResponse(CategoryResource::collection($data),'',200);
     }
 
     /**
@@ -29,15 +41,29 @@ class CategoryController extends Controller
      */
     public function store(StoreCategoryRequest $request)
     {
-        //
+        $details =[
+            'category' => $request->category
+        ];
+        DB::beginTransaction();
+        try{
+             $category = $this->categoryRepositoryInterface->store($details);
+
+             DB::commit();
+             return ApiResponseClass::sendResponse(new CategoryResource($category),'Category Create Successful',201);
+
+        }catch(\Exception $ex){
+            return ApiResponseClass::rollback($ex);
+        }
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Category $category)
+    public function show($id)
     {
-        //
+        $category = $this->categoryRepositoryInterface->getById($id);
+
+        return ApiResponseClass::sendResponse(new CategoryResource($category),'',200);
     }
 
     /**
@@ -51,16 +77,30 @@ class CategoryController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdateCategoryRequest $request, Category $category)
+    public function update(UpdateCategoryRequest $request, $id)
     {
-        //
+        $updateDetails =[
+            'category' => $request->category
+        ];
+        DB::beginTransaction();
+        try{
+             $category = $this->categoryRepositoryInterface->update($updateDetails,$id);
+
+             DB::commit();
+             return ApiResponseClass::sendResponse('Category Update Successful','',201);
+
+        }catch(\Exception $ex){
+            return ApiResponseClass::rollback($ex);
+        }
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Category $category)
+    public function destroy($id)
     {
-        //
+         $this->categoryRepositoryInterface->delete($id);
+
+        return ApiResponseClass::sendResponse('Category Delete Successful','',204);
     }
 }
