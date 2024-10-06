68e75ba5f71d5439779569dff423641737978bd7
Dev Server <dennisocana@gmail.com>
2024-10-06 18:00:45 -0400

    Added the repo scaffold

---
+++ serviciosTicketflow/app/Repositories/TaxRepository.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App;
+use App\Models\Tax;
+
+class TaxRepository
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Retrieves all items.
+     */
+    public function index()
+    {
+        return Tax::all();
+    }
+
+    /**
+     * Retrieves an item by ID.
+     */
+    public function getById($id)
+    {
+        return Tax::findOrFail($id);
+    }
+
+    /**
+     * Creates a new item in the repository.
+     */
+    public function store(array $data)
+    {
+        return Tax::create($data);
+    }
+
+    /**
+     * Updates an Item by ID.
+     */
+    public function update(array $data, $id)
+    {
+        return Tax::whereId($id)->update($data);
+    }
+
+    /**
+     * Deletes an Item by ID.
+     */
+    public function delete($id)
+    {
+        Tax::destroy($id);
+    }
+
+}
