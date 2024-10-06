c37c4b327ba7bb9dfe2a0bc715d0fd0d92841e04
Dev Server <dennisocana@gmail.com>
2024-10-06 18:13:29 -0400

    added a unique tag to the description column

--- serviciosTicketflow/database/migrations/2024_10_06_205149_create_taxes_table.php
+++ serviciosTicketflow/database/migrations/2024_10_06_205149_create_taxes_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('taxes', function (Blueprint $table) {
             $table->id();
-            $table->string('description',length:10);
+            $table->string('description',length:10)->unique();
             $table->decimal('value');
             $table->timestamps();
         });
