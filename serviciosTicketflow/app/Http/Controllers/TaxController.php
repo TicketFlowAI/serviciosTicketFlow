02af83af8e6ad501c40457672ffb6e5a17de64dd
Dev Server <dennisocana@gmail.com>
2024-10-06 18:09:52 -0400

    Added a scaffold controller and modified it to match a base structure with crud functionality

--- serviciosTicketflow/app/Http/Controllers/TaxController.php
+++ serviciosTicketflow/app/Http/Controllers/TaxController.php
@@ -2,18 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Tax;
 use App\Http\Requests\StoreTaxRequest;
 use App\Http\Requests\UpdateTaxRequest;
-use App\Models\Tax;
-
+use App\Interfaces\TaxRepositoryInterface;
+use App\Classes\ApiResponseClass;
+use App\Http\Resources\TaxResource;
+use Illuminate\Support\Facades\DB;
 class TaxController extends Controller
 {
+    
+    private TaxRepositoryInterface $taxRepositoryInterface;
+    
+    public function __construct(TaxRepositoryInterface $taxRepositoryInterface)
+    {
+        $this->taxRepositoryInterface = $taxRepositoryInterface;
+    }
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        //
+        $data = $this->taxRepositoryInterface->index();
+
+        return ApiResponseClass::sendResponse(TaxResource::collection($data),'',200);
     }
 
     /**
@@ -29,15 +41,30 @@ class TaxController extends Controller
      */
     public function store(StoreTaxRequest $request)
     {
-        //
+        $details =[
+            'description' => $request->description,
+            'value' => $request->value
+        ];
+        DB::beginTransaction();
+        try{
+             $tax = $this->taxRepositoryInterface->store($details);
+
+             DB::commit();
+             return ApiResponseClass::sendResponse(new TaxResource($tax),'Tax Create Successful',201);
+
+        }catch(\Exception $ex){
+            return ApiResponseClass::rollback($ex);
+        }
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Tax $tax)
+    public function show($id)
     {
-        //
+        $tax = $this->taxRepositoryInterface->getById($id);
+
+        return ApiResponseClass::sendResponse(new TaxResource($tax),'',200);
     }
 
     /**
@@ -51,16 +78,31 @@ class TaxController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdateTaxRequest $request, Tax $tax)
+    public function update(UpdateTaxRequest $request, $id)
     {
-        //
+        $updateDetails =[
+            'description' => $request->description,
+            'value' => $request->value
+        ];
+        DB::beginTransaction();
+        try{
+             $tax = $this->taxRepositoryInterface->update($updateDetails,$id);
+
+             DB::commit();
+             return ApiResponseClass::sendResponse('Tax Update Successful','',201);
+
+        }catch(\Exception $ex){
+            return ApiResponseClass::rollback($ex);
+        }
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Tax $tax)
+    public function destroy($id)
     {
-        //
+         $this->taxRepositoryInterface->delete($id);
+
+        return ApiResponseClass::sendResponse('Tax Delete Successful','',204);
     }
 }
