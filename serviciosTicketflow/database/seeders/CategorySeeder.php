5c48a1bfcd76f85a724fd8fca9fb0e6391b34c22
Dev Server <dennisocana@gmail.com>
2024-10-06 18:37:40 -0400

    added a seeder for category which creates the necessary db registries for the class

--- serviciosTicketflow/database/seeders/CategorySeeder.php
+++ serviciosTicketflow/database/seeders/CategorySeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,17 @@ class CategorySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Category::create([
+            'category' => 'Dominios'
+        ]);
+        Category::create([
+            'category' => 'Hosting'
+        ]);
+        Category::create([
+            'category' => 'Licencias'
+        ]);
+        Category::create([
+            'category' => 'Mantenimiento/Soporte continuo'
+        ]);
     }
 }
