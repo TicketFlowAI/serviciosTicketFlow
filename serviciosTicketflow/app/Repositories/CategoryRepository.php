a7fe9e382bbac65910a5d53e0cea7ad89c1111cc
Dev Server <dennisocana@gmail.com>
2024-10-06 18:19:28 -0400

    added category repository by scaffolding

---
+++ serviciosTicketflow/app/Repositories/CategoryRepository.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Repositories;
+use App\Interfaces\Interfaces\CategoryRepositoryInterface;
+use App\Models\Category;
+
+class CategoryRepository implements CategoryRepositoryInterface
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Retrieves all items.
+     */
+    public function index()
+    {
+        return Category::all();
+    }
+
+    /**
+     * Retrieves an item by ID.
+     */
+    public function getById($id)
+    {
+        return Category::findOrFail($id);
+    }
+
+    /**
+     * Creates a new item in the repository.
+     */
+    public function store(array $data)
+    {
+        return Category::create($data);
+    }
+
+    /**
+     * Updates an Item by ID.
+     */
+    public function update(array $data, $id)
+    {
+        return Category::whereId($id)->update($data);
+    }
+
+    /**
+     * Deletes an Item by ID.
+     */
+    public function delete($id)
+    {
+        Category::destroy($id);
+    }
+}
