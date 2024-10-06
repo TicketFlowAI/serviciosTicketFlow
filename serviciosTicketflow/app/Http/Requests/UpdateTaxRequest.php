5b8daf678836deaea2db79b3fb9daa90d434a80d
Dev Server <dennisocana@gmail.com>
2024-10-06 18:05:06 -0400

    Added request validations for both store and update functions

--- serviciosTicketflow/app/Http/Requests/UpdateTaxRequest.php
+++ serviciosTicketflow/app/Http/Requests/UpdateTaxRequest.php
@@ -3,6 +3,8 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Contracts\Validation\Validator;
 
 class UpdateTaxRequest extends FormRequest
 {
@@ -11,7 +13,7 @@ class UpdateTaxRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +24,17 @@ class UpdateTaxRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'description' => 'required|string|unique',
+            'value' => 'required|numeric'
         ];
+    }
+
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'success'   => false,
+            'message'   => 'Validation errors',
+            'data'      => $validator->errors()
+        ]));
     }
 }
