b3a5289ac9126ba57012cad219719ea9d4506430
Dev Server <dennisocana@gmail.com>
2024-10-06 20:52:00 -0400

    created service contracts to store the services contracted by each company

---
+++ serviciosTicketflow/app/Http/Controllers/ServiceContractController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreServiceContractRequest;
+use App\Http\Requests\UpdateServiceContractRequest;
+use App\Models\ServiceContract;
+
+class ServiceContractController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreServiceContractRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ServiceContract $serviceContract)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(ServiceContract $serviceContract)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateServiceContractRequest $request, ServiceContract $serviceContract)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ServiceContract $serviceContract)
+    {
+        //
+    }
+}
