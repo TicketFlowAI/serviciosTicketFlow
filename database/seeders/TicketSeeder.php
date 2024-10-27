491a881e496caec1b896f69cdc704cb11e8a65d8
Dev Server <dennisocana@gmail.com>
2024-10-27 16:16:19 -0400

    added a ticket seeder with 3 sample tickets

--- database/seeders/TicketSeeder.php
+++ database/seeders/TicketSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Ticket;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,26 @@ class TicketSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Ticket::create([
+            'service_contract_id' => 1,
+            'title' => 'Errores con los correos',
+            'priority' => 1,
+            'needsHumanInteraction' => '1',
+            'user_id' => 2
+        ]);
+        Ticket::create([
+            'service_contract_id' => 2,
+            'title' => 'Como conectar una cuenta de correo',
+            'priority' => 3,
+            'needsHumanInteraction' => '0',
+            'user_id' => 2
+        ]);
+        Ticket::create([
+            'service_contract_id' => 3,
+            'title' => 'Necesito espacio en mi hosting',
+            'priority' => 2,
+            'needsHumanInteraction' => '1',
+            'user_id' => 2
+        ]);
     }
 }
