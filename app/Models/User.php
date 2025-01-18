e8dcf1543496e71ed8f2c26d5549e045295787eb
Dev Server <dennisocana@gmail.com>
2025-01-17 21:20:17 -0500

    Add TwoFactorAuthenticatable trait to User model for enhanced security

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,13 +8,14 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Fortify\TwoFactorAuthenticatable;
 use Laravel\Sanctum\HasApiTokens;
 use Spatie\Permission\Traits\HasRoles;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable, HasApiTokens, HasRoles, SoftDeletes;
+    use HasFactory, Notifiable, HasApiTokens, HasRoles, SoftDeletes; //TwoFactorAuthenticatable;
 
     /**
      * The attributes that are mass assignable.
